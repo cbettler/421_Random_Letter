ae220a5a5c49c18023112a4ad04a9a1a7072ab72
cbettler <carlee.bettler@gmail.com>
2019-11-22 19:07:56 -0600

    Add files via upload

---
+++ R/hello.R
@@ -0,0 +1,21 @@
+#' @title Test
+#'
+#' @description Test
+#'
+#' @param symbol
+#'
+#' @return NULL
+#'
+#' @examples code('A')
+#'
+#' @export
+#'
+
+cat_function <- function(love=TRUE){
+  if(love==TRUE){
+    print("I love cats!")
+  }
+  else {
+    print("I am not a cool person.")
+  }
+}
---
+++ R/morecats.R
@@ -0,0 +1,23 @@
+#' @title Test
+#'
+#' @description Test
+#'
+#' @param symbol
+#'
+#' @return NULL
+#'
+#' @examples code('A')
+#'
+#' @export
+#'
+
+cat_function2 <- function(love=TRUE){
+  if(love==TRUE){
+    print("I love cats!")
+  }
+  else {
+    print("I am not a cool person.")
+  }
+}
+
+
